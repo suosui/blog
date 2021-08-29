c46164f25f23c838a6cdcd71f4b9f20341ed5b17
suosui <18844141934@163.com>
2021-08-29 11:44:06 +0800

    init

---
+++ examples/redis/segmentLock/mongoClient.js
@@ -0,0 +1,44 @@
+const { MongoClient } = require('mongodb');
+const blubird = require('bluebird')
+
+const dbConfig = {
+  connectionString: process.env.DB_STRING || 'mongodb://127.0.0.1:27017/local',
+  connectHost: process.env.DB_HOST || 'mongodb://127.0.0.1:27017',
+  dbname: process.env.DB_NAME || 'local',
+  username: process.env.DB_USERNAME || '',
+  password: process.env.DB_PASSWORD || '',
+}
+const mongoPromise = blubird.promisify(MongoClient.connect);
+
+let client;
+const connectDB = async () => {
+  try {
+    client = await mongoPromise(dbConfig.connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    const db = {
+      collection: name => {
+        return client.db(dbConfig.dbname).collection(name);
+      }
+    }
+    return db;
+  } catch (e) {
+    console.error(e);
+    throw new Error('Mongodb链接出错');
+  }
+}
+
+const closeDB = () => {
+  try {
+    client.close();
+  } catch (e) {
+    console.error(`error while closing clients`, e);
+  }
+}
+
+
+module.exports = {
+  closeDB: closeDB,
+  connectDB: connectDB
+}

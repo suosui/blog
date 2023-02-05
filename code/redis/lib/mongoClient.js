33c4342e5a2c54255322ac4a8b3079b6ae3de651
suosui <18844141934@163.com>
2023-02-05 10:36:09 +0800

    format

--- code/redis/lib/mongoClient.js
+++ code/redis/lib/mongoClient.js
@@ -2,44 +2,44 @@ const { MongoClient } = require('mongodb');
 const blubird = require('bluebird')
 
 const dbConfig = {
-  connectionString: process.env.DB_STRING || 'mongodb://127.0.0.1:27017/test',
-  connectHost: process.env.DB_HOST || 'mongodb://127.0.0.1:27017',
-  dbname: process.env.DB_NAME || 'test',
-  username: process.env.DB_USERNAME || '',
-  password: process.env.DB_PASSWORD || '',
+    connectionString: process.env.DB_STRING || 'mongodb://127.0.0.1:27017/test',
+    connectHost: process.env.DB_HOST || 'mongodb://127.0.0.1:27017',
+    dbname: process.env.DB_NAME || 'test',
+    username: process.env.DB_USERNAME || '',
+    password: process.env.DB_PASSWORD || '',
 }
 const mongoPromise = blubird.promisify(MongoClient.connect);
 
 let client;
 const connectDB = async () => {
-  try {
-    client = await mongoPromise(dbConfig.connectionString, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    const db = {
-      collection: name => {
-        return client.db(dbConfig.dbname).collection(name);
-      }
+    try {
+        client = await mongoPromise(dbConfig.connectionString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        const db = {
+            collection: name => {
+                return client.db(dbConfig.dbname).collection(name);
+            }
+        }
+        console.log(`Mongodb链接成功`);
+        return db;
+    } catch (e) {
+        console.error(e);
+        throw new Error('Mongodb链接出错');
     }
-    console.log(`Mongodb链接成功`);
-    return db;
-  } catch (e) {
-    console.error(e);
-    throw new Error('Mongodb链接出错');
-  }
 }
 
 const closeDB = () => {
-  try {
-    client.close();
-  } catch (e) {
-    console.error(`error while closing clients`, e);
-  }
+    try {
+        client.close();
+    } catch (e) {
+        console.error(`error while closing clients`, e);
+    }
 }
 
 
 module.exports = {
-  closeDB: closeDB,
-  connectDB: connectDB
+    closeDB: closeDB,
+    connectDB: connectDB
 }
